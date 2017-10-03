fcec34293b6f62ac221f13b42360aba75c4d8269
Tiarli Oliveira <tiarlioliveira@gmail.com>
2017-10-03 13:50:29 -0300

    feat(Position): add Position data object
    This commit introduces the new Position class, that will be
    resposible for holding the current position information, expressed
    in x_axis and y_axis that our upcoming ToyRobot will be in.

--- lib/awesome_toy_robot_simulator.rb
+++ lib/awesome_toy_robot_simulator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'awesome_toy_robot_simulator/version'
+require 'awesome_toy_robot_simulator/position'
 
 module AwesomeToyRobotSimulator
   # Your code goes here...
---
+++ lib/awesome_toy_robot_simulator/position.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module AwesomeToyRobotSimulator
+  class Position
+    attr_reader :x_axis, :y_axis
+
+    def initialize(x_axis, y_axis)
+      @x_axis = x_axis
+      @y_axis = y_axis
+    end
+  end
+end
